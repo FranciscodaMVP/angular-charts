91f6083a139c9afba21d7e673cdf9c1340708bc6
Francisco Valdez <isc.valdezfrancisco@gmail.com>
2017-10-10 13:19:57 -0700

    add modules, add component, add factory
    first archives, make index file

---
+++ chartComponent.js
@@ -0,0 +1,14 @@
+angular
+.module ('chartMaker', [])
+.controller('chartMakerController', chartMakerController)
+.factory('chartMakerService', chartMakerService);
+
+
+function chartMakerController($scope, chartMakerService) {
+    
+}
+
+
+function chartMakerService() {
+    
+}
