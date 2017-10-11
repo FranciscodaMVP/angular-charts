3387996759eac6ad5a700e6a1f51d4549ed64ae9
Francisco Valdez <isc.valdezfrancisco@gmail.com>
2017-10-11 13:28:51 -0700

    add directive
    change component service

--- chartComponent.js
+++ chartComponent.js
@@ -1,14 +1,33 @@
 angular
 .module ('chartMaker', [])
 .controller('chartMakerController', chartMakerController)
-.factory('chartMakerService', chartMakerService);
+//.factory('chartMakerService', chartMakerService);
+.directive('customChart', chart);
 
 
-function chartMakerController($scope, chartMakerService) {
-    
+function chartMakerController($scope, title, dataSource, type, width, height) {
+    $scope.title = title;
+    $scope.dataSource = dataSource;
+    $scope.type = type;
+    $scope.width = width;
+    $scope.height = height;
+    char($scope);
 }
 
+function chart($scope) {
+    var ctx = document.getElementById(originCanvas);
+    ctx.width = width;
+    ctx.height = height;
 
-function chartMakerService() {
-    
+    var theChart = new Chart (ctx, {
+        type: type,
+        datasets: [{
+            labal: title,
+            data: data,
+            borderWidth: 1
+        }]
+    })
+
+
 }
+
